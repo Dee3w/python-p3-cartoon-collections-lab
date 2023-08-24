e3b8c18a0d791194eea8cab2634b06a4e2319f44
Dee3w <135105714+Dee3w@users.noreply.github.com>
2023-08-24 14:46:53 +0300

    Completed assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    count = 1
+    for dwarf in list:
+        print(f"{count}. {dwarf}")
+        count += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    new_list = [(n.capitalize() + '!') for n in list]
+    return new_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    new_list = [n for n in list if len(n) > 4]
+    response = len(new_list) > 0
+    return response
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    Cheeses = ["cheddar", "gouda", "camembert"]
+    for n in list:
+        if n in Cheeses:
+            return n
+
+    return None
